a27099183f25e9dd4d7cf3587de558e917dd3c1c
Sanjay006-SM <smsanjaydmt@gmail.com>
2026-02-01 20:05:22 +0530

    Change API base URL to production server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://car-rental-phho.onrender.com',
 });
 
 // Add a request interceptor to attach JWT token
